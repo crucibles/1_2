dc0bc8cce6504f0a4226c47e78249a3a363e89b7
crucibles <34654170+crucibles@users.noreply.github.com>
2019-02-21 19:53:49 +0800

    rating and many seats.

---
+++ ced/home.js
@@ -0,0 +1,26 @@
+
+
+var movie1 = {
+	title: "Alita",
+	price: 150.00,
+	cinema: 1,
+	date: "Feb 6, 2019",
+	synopsis: "Alita a restored cyborg that is strong and pretty."
+}
+
+var movie2 = {
+	title: "AquaMan",
+	price: 150.00,
+	cinema: 1,
+	date: "Feb 6, 2019",
+	synopsis: "Alita a restored cyborg that is strong and pretty."
+}
+
+var movies = new Array(movie1, movie2);
+
+
+
+function getMovies(){
+	
+
+}
